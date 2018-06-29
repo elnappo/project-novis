db522a796ee00ee0fdb6e74157e124116b7bc31f
Fabian Weisshaar <elnappo@nerdpol.io>
2018-06-29 18:57:55 +0200

    Update database model

--- project_novis/callsign/admin.py
+++ project_novis/callsign/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from .models import *
 
@@ -8,11 +9,56 @@ class BaseModelAdmin(admin.ModelAdmin):
     list_filter = ("created", "modified")
 
 
-@admin.register(Callsign)
-class CallsignAdmin(BaseModelAdmin):
-    list_display = ("name", "owner", "type")
+class LOTWUserInline(admin.TabularInline):
+    model = LOTWUser
+    verbose_name = "LOTW"
+    verbose_name_plural = "LOTW"
+
+
+class ClublogUserInline(admin.TabularInline):
+    model = ClublogUser
+    verbose_name = "Clublog"
+    verbose_name_plural = "Clublog"
+
+
+class ESQLUserInline(admin.TabularInline):
+    model = EQSLUser
+    verbose_name = "ESQL"
+    verbose_name_plural = "ESQL"
+
+
+def set_call_sign_metadata(modeladmin, request, queryset):
+    for callsign in queryset:
+        callsign.set_default_meta_data()
+        callsign.save()
+set_call_sign_metadata.short_description = "Set default metadata"
+
+
+@admin.register(CallSign)
+class CallSignAdmin(BaseModelAdmin):
+    list_display = ("name", "country", "owner", "type")
     list_display_links = ("name",)
     list_filter = ("type", "issued", "created", "modified")
+    search_fields = ("name",)
+    actions = [set_call_sign_metadata]
+
+    fieldsets = (
+        ('Identifier', {
+            'fields': (('name', 'prefix'), 'country')
+        }),
+        ('Details', {
+            'fields': ('type', 'owner', 'active', "issued", "comment")
+        }),
+        ('Location', {
+            'fields': ('cq_zone', 'itu_zone', 'itu_region', 'grid', 'latitude', 'longitude')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('created', 'modified')
+        }),
+    )
+    raw_id_fields = ("owner", "prefix")
+    inlines = [LOTWUserInline, ClublogUserInline, ESQLUserInline]
 
 
 @admin.register(DMRID)
@@ -27,8 +73,28 @@ class ClubAdmin(BaseModelAdmin):
     list_display = ("callsign", "owner")
     list_display_links = ("callsign",)
 
+    raw_id_fields = ("callsign", "owner", "members")
+
 
 @admin.register(Country)
 class CountryAdmin(BaseModelAdmin):
-    list_display = ("name", "cq_zone", "itu_zone")
+    list_display = ("name", "alpha_2", "alpha_3", "numeric_3")
     list_display_links = ("name",)
+    ordering = ('name',)
+
+
+@admin.register(DXCCEntry)
+class DXCCEntryAdmin(BaseModelAdmin):
+    list_display = ("id", "name", "country")
+    list_display_links = ("id", "name")
+    list_filter = ("deleted",)
+    ordering = ('id',)
+
+
+@admin.register(CallSignPrefix)
+class CallsignPrefixAdmin(BaseModelAdmin):
+    list_display = ("name", "dxcc", "country")
+    list_display_links = ("name",)
+    list_filter = ("country", "cq_zone", "itu_zone")
+    ordering = ('name',)
+    search_fields = ("name",)
--- project_novis/callsign/models.py
+++ project_novis/callsign/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from .utils import CallSignField, CQZoneField, ITUZoneField, ITURegionField, QTHLocatorField
+
+CALL_SIGN_TYPE_CHOICES = (
+    ("personal", _("Personal")),
+    ("club", _("Club")),
+    ("repeater", _("Repeater")),
+    ("beacon", _("Beacon")),
+    ("educational", _("Educational")),
+    ("experimental", _("Experimental")),
+    ("special_event", _("Special Event")),
+    ("shortwave_listener", _("Shortwave Listener"))
+)
+
+CONTINENT_CHOICES = (
+    ("AF", _("Asia")),
+    ("AN", _("Antarctica")),
+    ("AS", _("Africa")),
+    ("EU", _("Europe")),
+    ("NA", _("North America")),
+    ("OC", _("Oceania")),
+    ("SA", _("South America"))
+)
 
 
 class BaseModel(models.Model):
@@ -13,49 +36,92 @@ class BaseModel(models.Model):
 
 
 class Country(BaseModel):
-    CONTINENT_CHOICES = (
-        ("AF", _("Asia")),
-        ("AN", _("Antarctica")),
-        ("AS", _("Africa")),
-        ("EU", _("Europe")),
-        ("NA", _("North America")),
-        ("OC", _("Oceania")),
-        ("SA", _("South America"))
-    )
-
-    name = models.CharField(max_length=64, db_index=True)
-    adif = models.PositiveSmallIntegerField(db_index=True)
-    cq_zone = models.PositiveSmallIntegerField(_("CQ zone"),
-                                               validators=[MinValueValidator(1), MaxValueValidator(40)])
-    itu_zone = models.PositiveSmallIntegerField(_("ITU zone"),
-                                                validators=[MinValueValidator(1), MaxValueValidator(75)])
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    continent = models.CharField(choices=CONTINENT_CHOICES, max_length=2)
-    utc_offset = models.SmallIntegerField(_("UTC offset"), validators=[MinValueValidator(-12), MaxValueValidator(15)])
+    name = models.CharField(max_length=64, unique=True, db_index=True)
+    adif_name = models.CharField(max_length=64, db_index=True, blank=True)
+    alpha_2 = models.CharField(max_length=2, unique=True, db_index=True)
+    alpha_3 = models.CharField(max_length=3, unique=True, db_index=True)
+    numeric_3 = models.CharField(max_length=3, unique=True, db_index=True)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Callsign(BaseModel):
-    TYPE_CHOICES = (
-        ("personal", _("Personal")),
-        ("club", _("Club")),
-        ("repeater", _("Repeater")),
-        ("beacon", _("Beacon")),
-        ("educational", _("Educational")),
-        ("experimental", _("Experimental")),
-        ("special_event", _("Special Event")),
-        ("shortwave_listener", _("Shortwave Listener"))
-    )
+class DXCCEntry(BaseModel):
+    name = models.CharField(max_length=64, db_index=True)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, blank=True)
+    deleted = models.BooleanField(default=False)
 
-    name = models.CharField(max_length=8)
-    country = models.ForeignKey(Country, on_delete=models.PROTECT)
-    type = models.CharField(choices=TYPE_CHOICES, max_length=32, blank=True)
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "DXCC Entry"
+        verbose_name_plural = "DXCC Entries"
+        unique_together = ("name", "deleted")
+
+
+class CallSignPrefix(BaseModel):
+    name = models.CharField(max_length=16, unique=True, db_index=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, null=True, blank=True)
+    dxcc = models.ForeignKey(DXCCEntry, on_delete=models.PROTECT, null=True, blank=True)
+    cq_zone = CQZoneField(null=True, blank=True)
+    itu_zone = ITUZoneField(null=True, blank=True)
+    itu_region = ITURegionField(null=True, blank=True)
+    continent = models.CharField(choices=CONTINENT_CHOICES, max_length=2, blank=True)
+    latitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
+    utc_offset = models.FloatField(null=True, blank=True)
+    type = models.CharField(choices=CALL_SIGN_TYPE_CHOICES, max_length=32, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CallSignManager(models.Manager):
+    def create_call_sign(self, call_sign: str):
+        call_sign = self.create(name=call_sign)
+        call_sign.set_default_meta_data()
+        call_sign.save()
+        return call_sign
+
+
+class CallSign(BaseModel):
+    name = CallSignField(unique=True, db_index=True)
+    prefix = models.ForeignKey(CallSignPrefix, on_delete=models.PROTECT, null=True, blank=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, null=True, blank=True)
+    cq_zone = CQZoneField(null=True, blank=True)
+    itu_zone = ITUZoneField(null=True, blank=True)
+    itu_region = ITURegionField(null=True, blank=True)
+    grid = QTHLocatorField(blank=True)
+    latitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
+    type = models.CharField(choices=CALL_SIGN_TYPE_CHOICES, max_length=32, blank=True)
     owner = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
     active = models.BooleanField(default=True)
     issued = models.DateField(null=True, blank=True)
+
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name="created_by")
+    comment = models.TextField(blank=True)
+
+    objects = CallSignManager()
+
+    def set_default_meta_data(self):
+        call_sign = self.name
+        while call_sign:
+            if CallSignPrefix.objects.filter(name=call_sign).exists():
+                prefix = CallSignPrefix.objects.get(name=call_sign)
+                self.prefix = prefix
+                self.country = prefix.country
+                self.cq_zone = prefix.cq_zone
+                self.itu_zone = prefix.itu_zone
+                self.latitude = prefix.latitude
+                self.longitude = prefix.longitude
+                break
+            else:
+                call_sign = call_sign[:-1]
+
+    def get_absolute_url(self):
+        return reverse('callsign:callsign-html-detail', args=[self.name])
 
     def __str__(self) -> str:
         return self.name
@@ -63,7 +129,7 @@ class Callsign(BaseModel):
 
 class DMRID(BaseModel):
     name = models.PositiveSmallIntegerField()
-    callsign = models.ForeignKey(Callsign, on_delete=models.SET_NULL, null=True, blank=True)
+    callsign = models.ForeignKey(CallSign, on_delete=models.SET_NULL, null=True, blank=True)
     active = models.BooleanField(default=True)
     issued = models.DateTimeField(null=True, blank=True)
 
@@ -75,7 +141,7 @@ class DMRID(BaseModel):
 
 
 class Club(BaseModel):
-    callsign = models.ForeignKey(Callsign, on_delete=models.PROTECT)
+    callsign = models.ForeignKey(CallSign, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     owner = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
     members = models.ManyToManyField(get_user_model(), related_name="members")
@@ -86,17 +152,26 @@ class Club(BaseModel):
 
 
 class LOTWUser(BaseModel):
-    callsign = models.OneToOneField(Callsign, on_delete=models.CASCADE)
-    last_activity = models.DateTimeField()
+    callsign = models.OneToOneField(CallSign, on_delete=models.CASCADE)
+    lotw_last_activity = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.callsign.name
 
 
 class ClublogUser(BaseModel):
-    callsign = models.OneToOneField(Callsign, on_delete=models.CASCADE)
-    first_qso = models.DateTimeField()
-    last_qso = models.DateTimeField()
-    last_upload = models.DateTimeField()
-    oqrs = models.BooleanField()
+    callsign = models.OneToOneField(CallSign, on_delete=models.CASCADE)
+    clublog_first_qso = models.DateTimeField()
+    clublog_last_qso = models.DateTimeField()
+    clublog_last_upload = models.DateTimeField()
+    clublog_oqrs = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.callsign.name
 
 
-class ESQLUser(BaseModel):
-    callsign = models.OneToOneField(Callsign, on_delete=models.CASCADE)
+class EQSLUser(BaseModel):
+    callsign = models.OneToOneField(CallSign, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.callsign.name
