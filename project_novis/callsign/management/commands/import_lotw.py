b6d1826861b9246bb2092a29d58584c2e5461c17
Fabian Weisshaar <elnappo@nerdpol.io>
2018-06-29 12:43:47 +0200

    Set call sign metadata on LOTW import

--- project_novis/callsign/management/commands/import_lotw.py
+++ project_novis/callsign/management/commands/import_lotw.py
@@ -5,7 +5,7 @@ from dateutil.parser import parse
 from django.core.management.base import BaseCommand
 import requests
 
-from ...models import Callsign
+from ...models import CallSign, LOTWUser
 
 
 class Command(BaseCommand):
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         counter = 0
-        new_callsign_counter = 0
+        new_counter = 0
         r = requests.get(options['url'], stream=True)
 
         if r.status_code == 200:
@@ -25,10 +25,19 @@ class Command(BaseCommand):
                 valid = re.match('^[\w]+$', row[0]) is not None
                 if valid:
                     counter += 1
-                    callsign, new_callsign = Callsign.objects.get_or_create(name=row[0])
-                    callsign.last_activity = parse(row[1]+"T"+row[2]+"Z")
-                    if new_callsign:
-                        new_callsign_counter += 1
+
+                    call_sign_instance, new_call_sign = CallSign.objects.get_or_create(name=row[0])
+                    if new_call_sign:
+                        call_sign_instance.set_default_meta_data()
+                        call_sign_instance.save()
+                        new_counter += 1
+
+                    last_activity = parse(row[1] + "T" + row[2] + "Z")
+                    lotw_instance, lotw_instance_created = LOTWUser.objects.get_or_create(callsign=call_sign_instance, defaults={"callsign": call_sign_instance,
+                                                                                                                                 "lotw_last_activity": last_activity})
+                    if not lotw_instance_created and lotw_instance.lotw_last_activity < last_activity:
+                        lotw_instance.lotw_last_activity = last_activity
+                        lotw_instance.save()
 
             self.stdout.write(self.style.SUCCESS('Successfully imported %d users from which %d are new from %s' % (
-                counter, new_callsign_counter, options['url'])))
+                counter, new_counter, options['url'])))
