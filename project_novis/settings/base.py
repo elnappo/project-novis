ccc9580c6735760aa8c57a9e0a43e3ba0c268b9b
Fabian Weisshaar <elnappo@nerdpol.io>
2018-11-16 16:16:11 +0100

    Add django.middleware.clickjacking.XFrameOptionsMiddleware

--- project_novis/settings/base.py
+++ project_novis/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
