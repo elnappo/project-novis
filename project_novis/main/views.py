213306e9b4fef6dd3cf78c5ecbe0f73c9b048ca0
Fabian Weisshaar <elnappo@nerdpol.io>
2018-11-21 11:21:21 +0100

    Add management views, add robots.txt

--- project_novis/main/views.py
+++ project_novis/main/views.py
@@ -1,6 +1,36 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.cache import cache_page
 from django.views.generic.base import TemplateView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class EmptyView(View):
+    def get(self, request) -> HttpResponse:
+        return HttpResponse("")
+
+
+class VersionView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request) -> Response:
+        return Response({
+            "hostname": settings.HOSTNAME,
+            "version": settings.VERSION
+        })
 
 
 class HomePageView(TemplateView):
-
     template_name = "marketing.html"
+
+
+@method_decorator(cache_page(60 * 60), name='dispatch')
+class RobotsView(View):
+    def get(self, request) -> HttpResponse:
+        return HttpResponse(
+            "User-agent: *\nDisallow: /media/\nDisallow: /static/\nSitemap: https://www.project-novis.org/sitemap.xml",
+            content_type="text/plain")
