c484cc4e2b0eda1814f0ef10975baef8f2b42142
Fabian Weisshaar <elnappo@nerdpol.io>
2018-12-26 13:29:38 +0100

    Set rest framework default authentication classes

--- project_novis/settings/base.py
+++ project_novis/settings/base.py
@@ -126,6 +126,11 @@ OAUTH2_PROVIDER = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
