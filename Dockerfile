78f621259cd7e320c350ef6b994454e4354f9ec8
Fabian Weisshaar <elnappo@nerdpol.io>
2019-02-13 18:15:06 +0100

    Add Docker support, fix #15

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM ubuntu:18.04
+
+ENV PYTHONUNBUFFERED=1
+ENV VIRTUAL_ENV /venv
+ENV PATH /venv/bin:$PATH
+
+RUN groupadd --system app && useradd --no-log-init --system --gid app app
+
+RUN apt update && apt dist-upgrade --assume-yes && apt-get install --assume-yes --no-install-recommends\
+    bash \
+    git \
+    python3-minimal \
+    python3-venv \
+    locales \
+    # GeoDjango requirements
+    binutils \
+    libproj-dev \
+    gdal-bin \
+ && rm -rf /var/lib/apt/lists/* \
+ && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+ENV LANG en_US.utf8
+
+COPY requirements.txt /requirements.txt
+RUN apt update && apt-get install --assume-yes --no-install-recommends python3-dev libpq-dev gcc \
+ && python3 -m venv /venv \
+ && /venv/bin/pip install --upgrade pip \
+ && /venv/bin/pip install --upgrade --no-cache-dir --requirement /requirements.txt \
+ && apt purge --assume-yes python3-dev libpq-dev gcc \
+ && apt autoremove --assume-yes \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY . /code/
+WORKDIR /code/project_novis
+
+RUN python manage.py collectstatic --noinput
+
+USER app
+EXPOSE 8000
+
+ENTRYPOINT ["/code/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -o errexit    # abort script at first error
+set -o pipefail   # return the exit status of the last command in the pipe
+
+[[ -z "$DATABASE_URL" ]] && echo "ERROR: Need to set DATABASE_URL" && exit 1;
+
+# Define help message
+show_help() {
+    echo """
+Usage: docker run <imagename> COMMAND
+Commands
+bash            : Start a bash shell
+python          : Start a Python shell
+shell           : Start a Django Python shell
+test            : Run tests
+migrate         : Run database migrations
+createsuperuser : Create superuser
+manage          : Run manage.py task
+dev             : Start a normal Django development server
+gunicorn        : Run Gunicorn server
+help            : Show this message
+"""
+}
+
+# Run
+case "$1" in
+    bash)
+        exec /bin/bash "${@:2}"
+    ;;
+    python)
+        exec python "${@:2}"
+    ;;
+    shell)
+        exec python manage.py shell
+    ;;
+    test)
+        exec python manage.py test
+    ;;
+    migrate)
+        echo "Apply database migrations"
+        exec python manage.py migrate --noinput
+    ;;
+    createsuperuser)
+        [[ -z "$DJANGO_SUPERUSER_EMAIL" ]] && echo "ERROR: Need to set DJANGO_SUPERUSER_EMAIL" && exit 1;
+        [[ -z "$DJANGO_SUPERUSER_PASSWORD" ]] && echo "ERROR: Need to set DJANGO_SUPERUSER_PASSWORD" && exit 1;
+        echo "Create superuser ${DJANGO_SUPERUSER_EMAIL}"
+        exec python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser(\"$DJANGO_SUPERUSER_EMAIL\", \"$DJANGO_SUPERUSER_PASSWORD\")" || true
+    ;;
+    manage)
+        echo "Running Django management command ${@:2}"
+        exec python manage.py "${@:2}"
+    ;;
+    dev)
+        echo "Running Development Server on 0.0.0.0:8000"
+        exec python manage.py runserver 0.0.0.0:8000
+    ;;
+    gunicorn)
+        echo "Running App via Gunicorn"
+        exec gunicorn -b :8000 --worker-class gevent --worker-connections 20 --timeout 10 --graceful-timeout 30 --access-logfile - wsgi
+    ;;
+    help)
+        show_help
+        ;;
+    *)
+        echo "unknown command ${@:1}"
+        show_help
+        exit 1
+    ;;
+esac
