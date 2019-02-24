de2b8a2c92c3b022092ad413de5c1d794c4ace6f
Fabian Weisshaar <elnappo@nerdpol.io>
2019-02-24 19:16:48 +0100

    Respect callsign blacklist in ImportCommand, log imports into database

--- project_novis/callsign/management/commands/__init__.py
+++ project_novis/callsign/management/commands/__init__.py
@@ -1,9 +1,8 @@
-import time
-
-from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.utils import timezone
 
-from ...models import CallSign
+from ...models import CallSign, DataImport, CallsignBlacklist, Country
 from ...utils import extract_callsign
 
 
@@ -13,20 +12,29 @@ class ImportCommand(BaseCommand):
     """
 
     source = ""
+    task = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if not self.task:
+            raise LookupError("task class variable is empty")
 
-        self._start_time = time.time()
+        self._start_time = timezone.now()
         self._import_user_id = 1
         self._import_user = get_user_model().objects.get(id=self._import_user_id)
         self._callsign_counter = 0
         self._new_callsign_counter = 0
         self._update_callsign_counter = 0
         self._delete_callsign_counter = 0
+        self._invalid_callsign_counter = 0
+        self._blacklist_callsign_counter = 0
         self._error_counter = 0
         self._existing_callsigns = CallSign.objects.values_list('name', flat=True)
-
+        self._callsign_blacklist = CallsignBlacklist.objects.values_list('callsign', flat=True)
+        self._callsign_data_import_instance = DataImport.objects.create(start=self._start_time,
+                                                                        task=self.task,
+                                                                        description=self.help)
+        self.countries = dict(Country.objects.values_list("name", "id"))
         self.stdout.write(self.help)
 
     def _warning(self, message: str):
@@ -41,13 +49,24 @@ class ImportCommand(BaseCommand):
     def _write(self, message:str):
         self.stdout.write(message)
 
+    def handle(self, *args, **options):
+        """
+        The actual logic of the command. Subclasses must implement
+        this method.
+        """
+        raise NotImplementedError('subclasses of ImportCommand must provide a handle() method')
+
     def _handle_callsign(self, callsign: str, source: str = "", official: bool = False):
         self._callsign_counter += 1
         raw_callsign = extract_callsign(callsign)
 
         if not raw_callsign:
-            self._error_counter += 1
+            self._invalid_callsign_counter += 1
             raise ValueError(f"Invalid callsign {callsign}")
+
+        if raw_callsign in self._callsign_blacklist:
+            self._blacklist_callsign_counter += 1
+            raise ValueError(f"Blacklisted callsign {callsign}")
 
         elif raw_callsign not in self._existing_callsigns:
             call_sign_instance, new_call_sign = CallSign.objects.get_or_create(
@@ -55,7 +74,7 @@ class ImportCommand(BaseCommand):
                 defaults={"name": raw_callsign,
                           "created_by_id": self._import_user_id,
                           "source": source if source else self.source,
-                          "official_validated": official})
+                          "_official_validated": official})
             if new_call_sign:
                 self._new_callsign_counter += 1
                 call_sign_instance.set_default_meta_data()
@@ -68,12 +87,24 @@ class ImportCommand(BaseCommand):
             return call_sign_instance, False
 
     def _finish(self, extra_message: str = None):
-        self._success(f"duration: {time.time() - self._start_time} "
+        self._callsign_data_import_instance.callsigns = self._callsign_counter
+        self._callsign_data_import_instance.new_callsigns = self._new_callsign_counter
+        self._callsign_data_import_instance.updated_callsigns = self._update_callsign_counter
+        self._callsign_data_import_instance.deleted_callsigns = self._delete_callsign_counter
+        self._callsign_data_import_instance.invalid_callsigns = self._invalid_callsign_counter
+        self._callsign_data_import_instance.blacklisted_callsigns = self._blacklist_callsign_counter
+        self._callsign_data_import_instance.errors = self._error_counter
+        self._callsign_data_import_instance.success = True
+        self._callsign_data_import_instance.stop = timezone.now()
+        self._callsign_data_import_instance.save()
+
+        self._success(f"duration: {timezone.now() - self._start_time} "
                       f"callsings: {self._callsign_counter} "
                       f"new callsings: {self._new_callsign_counter} "
                       f"update callsings: {self._update_callsign_counter} "
                       f"delete callsings: {self._delete_callsign_counter} "
+                      f"invalid callsigns: {self._invalid_callsign_counter} "
+                      f"blacklisted callsigns: {self._blacklist_callsign_counter} "
                       f"error callsings: {self._error_counter}")
         if extra_message:
             self._success(extra_message)
-
