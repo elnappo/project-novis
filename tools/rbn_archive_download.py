f7c283bd9d772f36b9745114a3625d092870d9ce
Fabian Weisshaar <elnappo@nerdpol.io>
2019-03-12 21:13:44 +0100

    Add rbn raw data download script

---
+++ tools/rbn_archive_download.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import os
+
+
+# Download raw data from reverse beacon network and save under rbn/
+html_page = urllib.request.urlopen("http://www.reversebeacon.net/raw_data/")
+soup = BeautifulSoup(html_page)
+
+for link in soup.findAll('a', attrs={'href': re.compile(r"dl\.php\?f=\d+")}):
+    url = f"http://www.reversebeacon.net/raw_data/{link.get('href')}"
+    filename = f"rbn/{link.get('href').split('=')[1]}.zip"
+    if not os.path.isfile(filename):
+        print(f"Downloading: {url}")
+        urllib.request.urlretrieve(url, filename+".tmp")
+        os.rename(filename+".tmp", filename)
